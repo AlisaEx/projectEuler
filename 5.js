1b96ab294c7cc23c77a51d3e05efbe3d82ce52ac
AlisaEx <eatliightning@gmail.com>
2013-09-22 09:26:33 -0700

    exceeding maximum call stck, need to refactor

--- 5.js
+++ 5.js
@@ -4,27 +4,25 @@
 // What is the smallest positive number that is evenly divisible
 // by all of the numbers from 1 to 20?
 
-
-function findSmallestNumber(){
-	var n = 0;
-	while(!divisibilityCheck(n)){
-		console.log(n);
-		n+1;
+function findSmallestNumber(n,offset){
+	if(!divisibilityCheck(n,offset)){
+		return findSmallestNumber(n+1,offset);
 	}
-	return n;
+	else{
+		console.log(n);
+		return n;
+	}
 };
 
 
 
-function divisibilityCheck(number){
-	for(var i=1; i<21; i++){
-		if(number%i===0){
-			console.log(number);
-			return true;
-		}
-		else{
+function divisibilityCheck(number, checkTo){
+	for(var i = 1; i < checkTo+1; i++){
+		if(number%i != 0){
 			return false;
 		}
 	}
+	return true;
 };
-findSmallestNumber();
+findSmallestNumber(21,20);
+// console.log(divisibilityCheck(2520, 10));
